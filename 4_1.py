27baf8c0b9d400c34c8032d75d54c724cad73838
highlovllycd <128291208+ryzenazzx@users.noreply.github.com>
2025-06-30 14:26:41 +0300

    Delete 4_1.py

--- 4_1.py
+++
@@ -1,8 +0,0 @@
-# Сравнение двух введённых паролей
-password1 = input("Введите пароль: ")
-password2 = input("Подтвердите пароль: ")
-
-if password1 == password2:
-    print("Пароль принят")
-else:
-    print("Пароль не принят")
